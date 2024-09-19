77fa26f16d97501d388b263e8d4d2932d815f36a
tekunogosu <tekunogosu@eldritchhollows.com>
2024-09-18 17:07:49 -0700

    The plugin should now load its config without any config option set in the plugin manager.

--- theme-adhd.lua
+++ theme-adhd.lua
@@ -1,2 +1,9 @@
 local M = require("theme_adhd")
+
+-- call out setup ourselves so you don't have to give a config by default
+M.setup = M.setup or function(config)
+	config = config or {}
+	require("theme_adhd").setup(config)
+end
+
 return M
